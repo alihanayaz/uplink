39f38374c8023b0c45d57df1e39137aceb809934
alihanayaz <alihanayaz0@gmail.com>
2024-03-05 19:26:13 +0300

    implement login and logout functionality

--- uplink/urls.py
+++ uplink/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = "uplink"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", views.login, name="login"),
+    path("login", views.login_view, name="login"),
     path("account", views.account, name="account"),
+    path("logout", views.logout_view, name="logout"),
     path("user/<str:name>", views.profile, name="profile")
 ]
--- uplink/views.py
+++ uplink/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from .models import UserProfile
 
@@ -7,10 +8,31 @@ def index(request):
     return render(request, "index.html")
 
 def account(request):
-    return render(request, "account.html")
+    if not request.user.is_authenticated:
+        return redirect("uplink:login")
+    else:
+        return render(request, "account.html")
 
-def login(request):
-    return render(request, "login.html")
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('uplink:account')
+        else:
+            return render(request, "login.html", {
+                "message": "Invalid credentials."
+            })
+    else:
+        return render(request, "login.html")
+
+def logout_view(request):
+    logout(request)
+    return render(request, "login.html", {
+        "message": "Logged out."
+    })
 
 def profile(request, name):
     user = get_object_or_404(User, username=name)
